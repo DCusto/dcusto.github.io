4d0656e45a392430f25d04b966d9bd4361d16b1b
dhushyanth-s <dhushyanth5602@gmail.com>
2021-03-21 19:15:31 +0000

    Added numbers page (unresponsive)

--- scripts/main.ts
+++ scripts/main.ts
@@ -8,20 +8,3 @@ lottie.loadAnimation({
     loop: true,
     autoplay: true,
 });
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-var nav_placeholder = document.querySelector('.nav-placeholder');
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = () => {
-    if (window.pageYOffset > 0) {
-      navbar.classList.add("sticky")
-      nav_placeholder.classList.add('visible');
-      nav_placeholder.classList.remove('hidden');
-    } else {
-      navbar.classList.remove("sticky");
-      nav_placeholder.classList.add('hidden');
-      nav_placeholder.classList.remove('visible');
-    }
-}
